30d850d343dc1b0eb47be6e5f75aa3894edcd351
bkahadei <kagadey2@gmail.com>
2023-07-31 21:03:29 +0300

    Database changed to postgreSQL

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todo_app.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoser:123123123@localhost/todos'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal: sessionmaker[Session] = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
